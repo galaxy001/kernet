d1ee6b790049e1ceb41bffdc88b27821345726f0
Mike Chen <ccp0101@gmail.com>
2011-02-18 07:31:32 +0800

    rename kernet to kernet_kext

---
+++ kernet_cli/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  kernet_cli
+//
+//  Created by Mike Chen on 2/18/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
