36167a5f42dc1a49252cef4d9c793e4f2f6ceb3d
Mike Chen <ccp0101@gmail.com>
2011-09-25 16:11:33 +0800

    Kernet GUI target.

---
+++ Kernet/KernetAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  KernetAppDelegate.h
+//  Kernet
+//
+//  Created by Mike Chen on 9/25/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface KernetAppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
