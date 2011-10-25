ab71971a7c0cc9606ad862666593b7d9e0676c23
liruqi <liruqi@gmail.com>
2011-10-25 23:11:33 +0800

    flush

--- Scripts/domain-test.py
+++ Scripts/domain-test.py
@@ -16,11 +16,14 @@ for d in domains:
         print d + ": " + err
         if err.find("reset") >= 0:
             reset.write(d+"\n")
+            reset.flush()
             continue
         if err.find("timeout") >= 0:
             timeout.write(d+"\n")
+            timeout.flush()
             continue
         wtf.write(d + ": "+ err)
+        wtf.flush()
 
         
 
